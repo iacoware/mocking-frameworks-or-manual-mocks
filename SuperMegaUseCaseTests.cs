cc9ab1ae4a31354d2ba96fca09d365b5759ca526
Massimo Iacolare <info@massimoiacolare.it>
2018-05-29 08:34:51 +0200

    Added NoCustomer test

--- SuperMegaUseCaseTests.cs
+++ SuperMegaUseCaseTests.cs
@@ -13,13 +13,26 @@ namespace MockingFrameworkOrManualMocks
             var display = new Mock<IDisplay>();
 
             customerRepo.Setup(x => x.LoadAll()).Returns(new[] {"massimo", "matteo"});
-
             var superMega = new SuperMegaUseCase(customerRepo.Object, display.Object);
 
             superMega.DoSomething();
 
             display.Verify(d => d.Show("massimo"), Times.Once);
             display.Verify(d => d.Show("matteo"), Times.Once);
+        }
+
+        [Fact]
+        public void NoCustomer()
+        {
+            var customerRepo = new Mock<ICustomerRepo>();
+            var display = new Mock<IDisplay>();
+
+            customerRepo.Setup(x => x.LoadAll()).Returns(new string[0]);
+            var superMega = new SuperMegaUseCase(customerRepo.Object, display.Object);
+
+            superMega.DoSomething();
+
+            display.Verify(d => d.Show(It.IsAny<string>()), Times.Never);
         }
     }
 
