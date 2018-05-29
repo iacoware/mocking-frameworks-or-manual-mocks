e1b98070f459d78b1fcd2e01038feaa767904dfe
Massimo Iacolare <info@massimoiacolare.it>
2018-05-29 09:02:49 +0200

    Added ConsoleDisplayTest.ManyNames

---
+++ ConsoleDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MockingFrameworkOrManualMocks
+{
+    public class ConsoleDisplay : IDisplay
+    {
+        public void Show(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ ConsoleDisplayTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockingFrameworkOrManualMocks
+{
+    public class ConsoleDisplayTests : IDisposable
+    {
+        private TextWriter oldOut;
+        private StringWriter newOut;
+
+        public ConsoleDisplayTests()
+        {
+            newOut = new StringWriter();
+            oldOut = Console.Out;
+            Console.SetOut(newOut);
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(oldOut);
+            newOut.Dispose();
+        }
+
+        [Fact]
+        public void ManyNames()
+        {
+            var display = new ConsoleDisplay();
+
+            display.Show("first");
+            display.Show("second");
+
+            var output = newOut.ToString();
+            Assert.Contains("first", output);
+            Assert.Contains("second", output);
+        }
+    }
+}
