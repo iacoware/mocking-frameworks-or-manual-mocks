fae6fc8dc5de1baa34db27fc4830db99c0d00752
Massimo Iacolare <info@massimoiacolare.it>
2018-05-29 23:12:30 +0200

    Extract contract tests from FlatFileCustomerRepoTests

---
+++ CustomerRepoContractTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace MockingFrameworkOrManualMocks
+{
+    public abstract class CustomerRepoContractTests
+    {
+        protected abstract ICustomerRepo CreateWith(params string[] customers);
+        protected abstract ICustomerRepo CreateEmpty();
+
+        [Fact]
+        public void ManyCustomers()
+        {
+            var customerRepo = CreateWith("one", "two");
+
+            var all = customerRepo.LoadAll();
+
+            Assert.Contains<string>("one", all);
+            Assert.Contains<string>("two", all);
+        }
+
+        [Fact]
+        public void NoCustomers()
+        {
+            var customerRepo = CreateEmpty();
+
+            Assert.Throws<InvalidOperationException>(() => customerRepo.LoadAll());
+        }
+    }
+}
--- FlatFileCustomerRepoTests.cs
+++ FlatFileCustomerRepoTests.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using Xunit;
 
 namespace MockingFrameworkOrManualMocks
 {
-    public class FlatFileCustomerRepoTests
+    public class FlatFileCustomerRepoTests : CustomerRepoContractTests
     {
         private const string FileName = "customers.txt";
 
@@ -14,32 +12,13 @@ namespace MockingFrameworkOrManualMocks
             File.Delete(FileName);
         }
 
-        [Fact]
-        public void ManyCustomers()
-        {
-            var customerRepo = CreateWith("one", "two");
-
-            var all = customerRepo.LoadAll();
-
-            Assert.Contains("one", all);
-            Assert.Contains("two", all);
-        }
-
-        [Fact]
-        public void NoCustomers()
-        {
-            var customerRepo = CreateEmpty();
-
-            Assert.Throws<InvalidOperationException>(() => customerRepo.LoadAll());
-        }
-
-        private ICustomerRepo CreateWith(params string[] customers)
+        protected override ICustomerRepo CreateWith(params string[] customers)
         {
             File.WriteAllLines(FileName, customers);
             return new FlatFileCustomerRepo(FileName);
         }
 
-        private ICustomerRepo CreateEmpty()
+        protected override ICustomerRepo CreateEmpty()
         {
             File.WriteAllLines(FileName, new string[0]);
             return new FlatFileCustomerRepo(FileName);
