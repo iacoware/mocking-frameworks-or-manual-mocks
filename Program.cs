0af94315aec2f0515711a76be8ba0f64053c2593
Massimo Iacolare <info@massimoiacolare.it>
2018-05-29 09:06:26 +0200

    Our app is finally running with real dependencies

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace MockingFrameworkOrManualMocks
 {
@@ -10,6 +7,13 @@ namespace MockingFrameworkOrManualMocks
     {
         static void Main(string[] args)
         {
+            File.WriteAllLines("customers.txt", new []{"massimo", "matteo"});
+
+            var superMega = new SuperMegaUseCase(new FlatFileCustomerRepo("customers.txt"), new ConsoleDisplay());
+            superMega.DoSomething();
+
+            Console.WriteLine("\n\n\nPress any key to exit");
+            Console.ReadKey();
         }
     }
 }
