a89be160fb96827dc97ef6ee1c0f7b8d6d068a86
Massimo Iacolare <info@massimoiacolare.it>
2018-05-29 08:05:54 +0200

    Initial setup

---
+++ SuperMegaUseCaseTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace MockingFrameworkOrManualMocks
+{
+    public class SuperMegaUseCaseTests
+    {
+        [Fact]
+        public void ItWorks()
+        {
+            Assert.True(true);
+        }
+    }
+}
