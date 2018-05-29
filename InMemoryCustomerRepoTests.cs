95f0ce093e20445a0d5a6b92dbabe70585b5fa75
Massimo Iacolare <info@massimoiacolare.it>
2018-05-29 23:18:40 +0200

    Introduced InMemoryCustomerRepoTests and relative fake implementation. Notice how I'm forced to respect the contract thanks to my contract tests

---
+++ InMemoryCustomerRepoTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MockingFrameworkOrManualMocks
+{
+    public class InMemoryCustomerRepoTests : CustomerRepoContractTests
+    {
+        protected override ICustomerRepo CreateWith(params string[] customers)
+        {
+            return new InMemoryCustomerRepo(customers);
+        }
+
+        protected override ICustomerRepo CreateEmpty()
+        {
+            return new InMemoryCustomerRepo();
+        }
+    }
+
+    public class InMemoryCustomerRepo : ICustomerRepo
+    {
+        private readonly string[] customers;
+
+        public InMemoryCustomerRepo(params string[] customers)
+        {
+            this.customers = customers;
+        }
+
+        public string[] LoadAll()
+        {
+            if (customers.Length == 0) throw new InvalidOperationException();
+
+            return customers.ToArray();
+        }
+    }
+}
